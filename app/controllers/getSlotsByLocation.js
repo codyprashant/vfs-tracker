d6c5159db972dd2feabcc59a94c1d65acc7c4d96
Prashant Gupta <prashant.gurukul6893@gmail.com>
2022-08-24 22:08:41 +0530

    updating fileters

--- app/controllers/getSlotsByLocation.js
+++ app/controllers/getSlotsByLocation.js
@@ -19,10 +19,15 @@ const getSlots = async (req, res, next) => {
     let data1 = await LocationRecord.find({ sourceCountry: req.body.source,  destinationCountry: req.body.destination }).distinct('locationCode');
     console.log(data1)
 
-    for(let i = 0; i < data1.length; i++){
-      let data = await LocationRecord.find({ locationCode: data1[i], sourceCountry: req.body.source,  destinationCountry: req.body.destination }).sort({createdAt: 'descending'});
-      if(data) {
-        completeData.push(data[0])
+    let data2 = await LocationRecord.find({ sourceCountry: req.body.source,  destinationCountry: req.body.destination }).distinct('subCategory');
+    console.log(data2)
+    
+    for(let j = 0; j < data2.length; j++){
+      for(let i = 0; i < data1.length; i++){
+        let data = await LocationRecord.find({ locationCode: data1[i], subCategory:data2[j],  sourceCountry: req.body.source,  destinationCountry: req.body.destination }).sort({createdAt: 'descending'});
+        if(data) {
+          completeData.push(data[0])
+        }
       }
     }
     res.json({ message: completeData, status:"SUCCESS" });
